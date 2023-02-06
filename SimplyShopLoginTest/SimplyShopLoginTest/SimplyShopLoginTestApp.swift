bc20072ff2d9614b0df547ac0ac89d206d83999b
Dawson lu <39806416+dawsonlu@users.noreply.github.com>
2023-02-06 16:39:19 -0500

    Added SimplyShopLoginTest

---
+++ SimplyShopLoginTest/SimplyShopLoginTest/SimplyShopLoginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyShopLoginTestApp.swift
+//  SimplyShopLoginTest
+//
+//  Created by Dawson Lu on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct SimplyShopLoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
