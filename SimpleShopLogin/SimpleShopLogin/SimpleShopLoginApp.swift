45c68a34fed32b0daa770defc09563a6655143e3
Dawson lu <39806416+dawsonlu@users.noreply.github.com>
2023-02-06 16:24:42 -0500

    Added SimplyShop and SimplyShopLogin

---
+++ SimpleShopLogin/SimpleShopLogin/SimpleShopLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleShopLoginApp.swift
+//  SimpleShopLogin
+//
+//  Created by Dawson Lu on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleShopLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
